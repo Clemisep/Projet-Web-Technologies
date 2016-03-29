c80c47539c6b0738d8fcec55c91253464c4e7d02
yoanalfrede <alfyoman@hotmail.fr>
2016-03-29 10:03:58 +0200

    test

--- GAPP/src/projectgapp/ConnectionBDD.java
+++ GAPP/src/projectgapp/ConnectionBDD.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
-	public class Gapp {
+	public class ConnectionBDD {
 
 		public static void main(String[] args) {
 			
