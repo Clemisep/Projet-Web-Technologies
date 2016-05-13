9e5de2f68ee4634de9aec3297ac84c51c0ca0a40
rdubreui <root@Robin-PC>
2016-05-13 17:05:20 +0200

    Ajout de Template/

---
+++ Template/javaScript/getStudent.factory.spec.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('app', [])
+        .factory('getStudentFactory', getStudentFactory);
+        
+        getStudentFactory.$inject = ['$http', 'logger'];
+        
+        function getStudentFactory($http, logger) {
+            return { 
+                getStudent : getStudent
+            };
+            
+            function getStudent() {
+                return $http.get('findStudent.jsp')
+                    .then(getStudentComplete)
+                    .catch(getStudentFail);
+                    
+                    function getStudentComplete() {
+                        return response.data;
+                    }
+                    function getStudentFail() {
+                        logger.error('Fail to get student...');
+                    }
+            }
+        }
+
+})();
