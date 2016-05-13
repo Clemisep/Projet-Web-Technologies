52b98946bba1c51c26e3e4c18d55c1edce4138b1
root <root@Robin-PC>
2016-05-13 14:05:39 +0200

    adding Template

---
+++ Template/javaScript/skillManager.spec.js
@@ -0,0 +1,40 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('app', [])
+        .controller('skillManagerController', skillManagerController);
+
+    function skillManagerController() {
+        /*jshint validthis:true*/
+        var vm = this;
+
+        /*variables*/
+        vm.show = false;
+        vm.skillInput = null;
+        vm.subSkillInput = null;
+
+        /*functions*/
+        vm.showPanel = showPanel;
+        vm.hidePanel = hidePanel;
+        vm.addSkill = addSkill;
+        vm.addSubSkill = addSubSkill;
+
+        function showPanel() {
+            vm.show = true;
+            return vm.show;
+        }
+
+        function hidePanel() {
+            vm.show = false;
+            vm.subSkillInput = null;
+            vm.skillInput = null;
+            return vm.show;
+        }
+
+        function addSkill() {}
+
+        function addSubSkill() {}
+    }
+
+})();
---
+++ src/model/Note.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Note {
+	public String getDescription() {
+		return ""; // TODO
+	}
+	
+	public void setDescription(String description) {
+		// TODO
+	}
+	
+	public User getStudent() {
+		return null; // TODO
+	}
+}
