991ccc37cca320509f707bc98e63eb0f3d4213b2
Robin <robin@debian>
2016-06-01 18:35:19 +0200

    changes around newPassword.js

--- GAPP/WebContent/Template/javaScript/newPassword.js
+++ GAPP/WebContent/Template/javaScript/newPassword.js
@@ -1,29 +1,29 @@
 (function() {
-	'use strict';
-	
-	angular
-		.module('app')
-		.controller('newPassword', newPassword);
-	
-	newPassword.$inject = ['comparePasswordFactory'];
-	
-	function newPassword (comparePasswordFactory){
-		/*jshint validthis:true*/
-		var vm = this;
-	
-		vm.password = '';
-		vm.password2 = '';
-		vm.text = '';
-		
-		vm.activate = activate;
-		
-		activate(vm.password, vm.password2);
-		
-		function activate(password1, password2){
-			vm.text = comparePasswordFactory.compare(password1, password2);
-			return vm.text;
-		}
-		
-	}
-	
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('newPassword', newPassword);
+
+    newPassword.$inject = ['comparePasswordFactory'];
+
+    function newPassword (comparePasswordFactory){
+        /*jshint validthis:true*/
+        var vm = this;
+
+        vm.password = '';
+        vm.password2 = '';
+        vm.text = '';
+
+        vm.activate = activate;
+
+        activate(vm.password, vm.password2);
+
+        function activate(password1, password2){
+            vm.text = comparePasswordFactory.compare(password1, password2);
+            return vm.text;
+        }
+
+    }
+
 })();
